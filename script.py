0578ab2bff0affde49a72ede54558341917449fb
Brett Allen <nybrett8@gmail.com>
2019-06-04 16:34:27 -0400

    Removed Some Code

--- script.py
+++ script.py
@@ -1,16 +1,4 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    gretting = "Hello, {}".format(who_to_greet)
-    return gretting
 
 
 r = requests.get("https://coreyms.com")
